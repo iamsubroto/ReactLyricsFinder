bbb4d6392308da6d9287e2ee35d2b99925f66e5c
iamsubroto <smartcoder500@gmail.com>
2019-03-11 23:33:31 -0700

    Updates

---
+++ precache-manifest.6d530746f322d213838be0f681ae2e81.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b95289c60f17b81d29ced93c8db12f3b",
+    "url": "/ReactLyricsFinder/static/media/spinner.b95289c6.gif"
+  },
+  {
+    "revision": "bb6eab98745626326027",
+    "url": "/ReactLyricsFinder/static/js/runtime~main.bb6eab98.js"
+  },
+  {
+    "revision": "10af6c8f161f22b49f21",
+    "url": "/ReactLyricsFinder/static/js/main.10af6c8f.chunk.js"
+  },
+  {
+    "revision": "064baa1e741091572ff2",
+    "url": "/ReactLyricsFinder/static/js/2.064baa1e.chunk.js"
+  },
+  {
+    "revision": "10af6c8f161f22b49f21",
+    "url": "/ReactLyricsFinder/static/css/main.be3be9c3.chunk.css"
+  },
+  {
+    "revision": "296f4d8d90c208c5289f2c13c140abce",
+    "url": "/ReactLyricsFinder/index.html"
+  }
+];
